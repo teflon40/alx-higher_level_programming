9658200a7260d447877b6f4e072af61e71a90b9b
teflon40 <frimpong.bernard3220@gmail.com>
2023-10-15 18:56:45 +0000

    Handled only one argument passed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,13 @@
 
 if __name__ == '__main__':
     import sys
-    num_of_args = len(sys.argv) - 1
-    if num_of_args == 0:
-        print("{:d} arguments.".format(num_of_args))
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print("{:d} arguments.".format(num_args))
     else:
+        arg_str = "argument" if num_args == 1 else "arguments"
         i = 1
-        print("{:d} arguments:".format(num_of_args))
-        while i <= num_of_args:
+        print("{:d} {:s}:".format(num_args, arg_str))
+        while i <= num_args:
             print("{:d}: {:s}".format(i, sys.argv[i]))
             i += 1
