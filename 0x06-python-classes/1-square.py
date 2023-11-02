0a88c9e5e32ec863c45cc85b503972d2f8507d3c
teflon40 <frimpong.bernard3220@gmail.com>
2023-11-02 17:17:01 +0000

    Defines a square by size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Write a class Square that defines a square
+"""
+
+
+class Square:
+    """Defines a class square by attribute 'size'"""
+
+    def __init__(self, size):
+        """Initialize class attributes"""
+        self.__size = size
