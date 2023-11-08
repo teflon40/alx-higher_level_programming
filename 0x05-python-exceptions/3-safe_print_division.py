85dc1fac45d6c2b117ec84d8248a7b7649955e2e
teflon40 <frimpong.bernard3220@gmail.com>
2023-11-08 14:28:33 +0000

    Safe division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    quotient = None
+    try:
+        quotient = a / b
+    except ZeroDivisionError:
+        pass
+    finally:
+        print("Inside result: {}".format(quotient))
+    return quotient
