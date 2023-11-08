725a4fdf8c191421a44689ad420d558921fa9e91
teflon40 <frimpong.bernard3220@gmail.com>
2023-11-08 09:32:15 +0000

    Getters and Setters

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
+class Square:
+    """Defines a class by size"""
+
+    def __init__(self, size=0):
+        """
+            Initialize private instance attribute
+            args:
+                size (int): size of square
+            raises:
+                TypeError
+                ValueError
+        """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieves private instance attribute: size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area"""
+        return self.__size ** 2
