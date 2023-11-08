936f73bef8c34faa5c3568838563d6189da34f0f
teflon40 <frimpong.bernard3220@gmail.com>
2023-11-08 14:10:07 +0000

    IndexError

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(element), end='')
             num += 1
-        except (IndexError, TypeError, ValueError):
+        except (TypeError, ValueError):
             pass
     print('')
     return num
