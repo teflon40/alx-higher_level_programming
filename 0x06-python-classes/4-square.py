e172f68caea913d50a33e7f7b7ae932c933c01da
teflon40 <frimpong.bernard3220@gmail.com>
2023-11-08 10:57:59 +0000

    Getters and Setters

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -14,11 +14,7 @@ class Square:
                 TypeError
                 ValueError
         """
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
