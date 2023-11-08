ab489ec9023364869f8ea8c53eee86103c7b859f
teflon40 <frimpong.bernard3220@gmail.com>
2023-11-08 10:44:02 +0000

    right formatting

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -57,4 +57,4 @@ class Square:
         for _ in range(self.__position[1]):
             print('')
         for _ in range(self.__size):
-            print(' ' * self.__position[0], '#' * self.__size)
+            print(' ' * self.__position[0] + '#' * self.__size)
