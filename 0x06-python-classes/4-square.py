d6bca8aa8a34e27e09d8d81af7d789fb973d4ae1
teflon40 <frimpong.bernard3220@gmail.com>
2023-11-08 09:36:42 +0000

    Handle errors

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -27,6 +27,10 @@ class Square:
 
     @size.setter
     def size(self, value):
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
         self.__size = value
 
     def area(self):
