bd6d3384eb48e2d1da393139124ae2cb247e0841
teflon40 <frimpong.bernard3220@gmail.com>
2023-11-13 19:48:54 +0000

    rectangle class

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""class for rectangle"""
+from base import Base
+
+
+class Rectangle(Base):
+    """Model a rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """initialize attributes"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """retrieves private instance attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        """retrieves height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        """retrieves x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        """retrieves y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
