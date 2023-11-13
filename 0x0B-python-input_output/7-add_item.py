4fc38b6d242de092ef97d4d0ea3c60960bf5eddc
teflon40 <frimpong.bernard3220@gmail.com>
2023-11-13 18:36:48 +0000

    FileNotFoundError

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,6 +6,9 @@ from sys import argv
 save_to_json = __import__('5-save_to_json_file').save_to_json_file
 load_from_json = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
-arg_list = load_from_json(filename)
+try:
+    arg_list = load_from_json(filename)
+except FileNotFoundError:
+    arg_list = []
 arg_list.extend(argv[1:])
 save_to_json(arg_list, filename)
