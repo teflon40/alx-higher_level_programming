9e172c752803b5abb856a69c867b5716b6f94742
teflon40 <frimpong.bernard3220@gmail.com>
2023-11-13 20:33:40 +0000

    override __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -74,3 +74,7 @@ class Rectangle(Base):
         """Prints rectangle with #"""
         for _ in range(self.__height):
             print('#'*self.__width)
+
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} " + \
+                f"{self.__width}/{self.__height}"
