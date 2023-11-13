df92bb65b539383d6ad60cda6a7ca214098ab970
teflon40 <frimpong.bernard3220@gmail.com>
2023-11-13 20:12:59 +0000

    y must be >= 0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -63,5 +63,5 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError('y must be an integer')
         if value < 0:
-            raise ValueError('y must be > 0')
+            raise ValueError('y must be >= 0')
         self.__y = value
