0f21cb5fab6191e070ca3ace3e735b25e63947b0
teflon40 <frimpong.bernard3220@gmail.com>
2023-11-13 20:45:48 +0000

    modify display method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -72,8 +72,9 @@ class Rectangle(Base):
 
     def display(self):
         """Prints rectangle with #"""
+        print('\n'*self.__y, end="")
         for _ in range(self.__height):
-            print('#'*self.__width)
+            print(' '*self.__x + '#'*self.__width)
 
     def __str__(self):
         """override str representation"""
