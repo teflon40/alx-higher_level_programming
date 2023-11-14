bf16a6096efa005fca0fce27d4e78ec9304e5d69
teflon40 <frimpong.bernard3220@gmail.com>
2023-11-14 05:43:47 +0000

    from json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,6 +23,13 @@ class Base:
             return "[]"
         return json.dumps(list_dictionaries)
 
+    @staticmethod
+    def from_json_string(json_string):
+        """returns list of the JSON string representation"""
+        if json_string is None:
+            return '[]'
+        return json.loads(json_string)
+
     @classmethod
     def save_to_file(cls, list_objs):
         """writes JSON string to file"""
