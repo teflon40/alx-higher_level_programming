b7f92c64b8dbbd3b1a0426c3f6aa4f233fcd6870
teflon40 <frimpong.bernard3220@gmail.com>
2024-06-21 02:42:00 +0000

    Use triple equal

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,2 +1,4 @@
 #!/usr/bin/node
-if (process.argv.length < 3) { console.log('No argument'); } else { console.log('Argument found'); }
+if (process.argv.length < 3) { console.log('No argument'); }
+else if (process.argv.length === 3) { console.log('Argument found'); }
+else { console.log('Arguments found'); }
