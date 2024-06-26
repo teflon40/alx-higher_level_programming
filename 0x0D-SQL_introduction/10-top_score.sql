e6d44daa5669396e9da2ea4e068206d8799b8368
teflon40 <frimpong.bernard3220@gmail.com>
2024-06-26 11:52:18 -0700

    List all records of second_table in a certain order

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- Write a script that lists all records of the table second_table of a database
+/*	** Results should display both the score and the name
+	** Records should be ordered by score (top first)
+*/
+SELECT score, name FROM second_table
+ORDER BY score DESC;
