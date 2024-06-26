bec3bd87c29af4ed9d226e01503ec1fcdd7687b3
teflon40 <frimpong.bernard3220@gmail.com>
2024-06-26 11:59:56 -0700

    Lists all records with a score >= 10

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,7 @@
+-- Write a script that lists all records of with score >= 10 in the table second_table of a database
+/*	** Results should display both the score and the name
+	** Records should be ordered by score (top first)
+*/
+SELECT score, name FROM second_table
+WHERE score>=10
+ORDER BY score DESC;
