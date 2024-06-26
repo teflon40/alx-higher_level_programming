9bf6a8879111711d275327fc998b9478055289c6
teflon40 <frimpong.bernard3220@gmail.com>
2024-06-26 12:50:49 -0700

    score average of all records

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- Write a script that computes the score average of all records in the second_table of a database
+-- The result column name should be average
+SELECT AVG(score) AS average FROM second_table;
