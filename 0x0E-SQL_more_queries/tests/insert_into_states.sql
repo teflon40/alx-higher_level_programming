85379f9389cfc4b9a93bea14c8cc6aafb474165e
teflon40 <frimpong.bernard3220@gmail.com>
2024-06-28 14:24:36 -0700

    Automate testing

---
+++ 0x0E-SQL_more_queries/tests/insert_into_states.sql
@@ -0,0 +1,1 @@
+INSERT INTO states (name) VALUES ('california'), ('Arizona'), ('Texas'), ('Illinois'), ('Georgia'), ('Colorado');
---
+++ 0x0E-SQL_more_queries/tests/test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+DIRPATH="/root/alx-higher_level_programming/0x0E-SQL_more_queries/tests/"
+
+if [ $# -eq 1 ]
+then
+	TEST=$DIRPATH$1
+	cat $TEST | mysql -hlocalhost -uroot -p hbtn_0d_usa
+else
+	echo "Usage: $0 <sql file>"
+fi
