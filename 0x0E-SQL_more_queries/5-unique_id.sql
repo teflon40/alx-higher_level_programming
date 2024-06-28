f81f3e1c9282de521efe399b6d3e0b490383fa22
teflon40 <frimpong.bernard3220@gmail.com>
2024-06-28 12:01:51 -0700

    create a table and a column as unique

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,9 @@
+-- Write a script that creates the table unique_id on a MySQL server
+/*	unique_id description:
+		** id INT with the default value 1 and must be unique
+		** name VARCHAR(256)
+*/
+CREATE TABLE IF NOT EXISTS unique_id (
+	id INT DEFAULT 1 UNIQUE,
+	name VARCHAR(256)
+);
