32e3ccf5dca95e9ff515e93f16092bb63ca38a75
teflon40 <frimpong.bernard3220@gmail.com>
2024-06-29 10:37:40 -0700

    Query on hbtn_0d_tvshows database

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,10 @@
+/* Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked */
+
+--
+--	Each record should display tv_shows.title - tv_show_genres.genre_id
+--	Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+--
+
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
