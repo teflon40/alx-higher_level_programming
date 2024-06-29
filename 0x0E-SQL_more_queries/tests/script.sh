4a70a1d20256b2317825cd6d0fbc09b691781ed5
teflon40 <frimpong.bernard3220@gmail.com>
2024-06-29 09:45:16 -0700

    Small change

--- 0x0E-SQL_more_queries/tests/script.sh
+++ 0x0E-SQL_more_queries/tests/script.sh
@@ -5,7 +5,7 @@ DIRPATH="/root/alx-higher_level_programming/0x0E-SQL_more_queries/"
 if [ $# -eq 1 ]
 then
 	TEST=$DIRPATH$1
-	cat $TEST | mysql -hlocalhost -uroot -p
+	cat $TEST | mysql -hlocalhost -uroot -p hbtn_0d_usa
 else
 	echo "Usage: script.sh <sql file>"
 fi
