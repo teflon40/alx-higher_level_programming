bdb0dce2e96ad07efda0c13512f18dd53d9c200f
teflon40 <frimpong.bernard3220@gmail.com>
2024-06-29 11:38:47 -0700

    Added comments

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,3 +1,8 @@
+/* Write a script that lists all the genres of the show Dexter in the database */
+--
+-- Each record should display: tv_genres.name
+-- Results must be sorted in ascending order by the genre name
+--
 SELECT tv_genres.name FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
