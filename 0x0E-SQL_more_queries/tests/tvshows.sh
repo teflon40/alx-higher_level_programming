1211fe0d86e340c4fedd05da209807faf24177a7
teflon40 <frimpong.bernard3220@gmail.com>
2024-06-30 07:20:26 -0700

    Created test scripts for each database

---
+++ 0x0E-SQL_more_queries/tests/tvshows.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+DIRPATH="/root/alx-higher_level_programming/0x0E-SQL_more_queries/"
+
+if [ $# -eq 1 ]
+then
+	TEST=$DIRPATH$1
+	cat $TEST | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
+else
+	echo "Usage: script.sh <sql file>"
+fi
